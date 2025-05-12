ee21c5a2e351359d61694c55c003b3cb3649dc93
naldodj <marinaldo.jesus@gmail.com>
2025-05-11 22:42:33 -0300

    2025-05-11 22:42 UTC naldodj

--- src/tst/go64.bat
+++ src/tst/go64.bat
@@ -1,4 +1,4 @@
 @setlocal
 call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
-C:\Users\marin\Downloads\harbour_msvc2022_64_20250216\bin\win\msvc64\hbmk2 http_connector_tst_msvc.hbp -comp=msvc64
+F:\harbour_msvc2022_64_20250216\bin\win\msvc64\hbmk2 http_connector_tst_msvc.hbp -comp=msvc64
 @endlocal
